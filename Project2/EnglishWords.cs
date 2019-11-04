e929f5de2d32ff47ba02b4d664a3c0f9b83c9c25
Sarah Kollins <sarahkollinsse@gmail.com>
2019-11-04 15:35:59 -0500

    Initial commit

---
+++ Project2/EnglishWords.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+namespace Project2
+{
+        public class EnglishWords
+        {
+            List<string> englishWordsList = new List<string>();
+   
+           
+            StreamReader input;
+
+            // This constructor will initiate the loading of all words located
+            // in the given dictionary. The constructor must return very quickly,
+            // perhaps before the words have been completely loaded. Tasks will be
+            // needed to do this.
+            public EnglishWords()
+            {
+            var assembly = IntrospectionExtensions.GetTypeInfo(typeof(MainPage)).Assembly;
+            Stream stream = assembly.GetManifestResourceStream("Project2.words.txt");
+            using (var dictReader = new System.IO.StreamReader(stream))
+            {
+                while (!dictReader.EndOfStream) { 
+                string line = dictReader.ReadLine();
+                englishWordsList.Add(line);
+            }
+            }
+        
+        }
+
+       
+            //Loads the data from text file into a list
+            //Returns a task of type List
+            //Asynchronous method
+            public async Task<List<string>> LoadDictionaryAsync()
+            {
+                using (input)
+                {
+                    while (!input.EndOfStream)
+                    {
+                        string line = await input.ReadLineAsync();
+                    englishWordsList.Add(line);
+                    }
+                }
+                input.Close();
+                return englishWordsList;
+            }
+
+        // This method will return true only if the word appears in the
+        // dictionary. This method will need to wait, if it is called
+        // before the words have been completely loaded.
+        public bool isLegal(string word)
+        {
+            return englishWordsList.Contains(word);
+        }
+
+        //Sets the wordList to contain the list from .txt file
+        public List<String> getWordList()
+        {
+            return englishWordsList;
+        }
+
+
+        // This method will return an alphabetically sorted list of all words that
+        // can be formed using a collection of letters and having the given length.
+        // Each letter can only be used once. For example, consider length 5 words
+        // the collection of letters: HERATH
+        // Here are some of the words that can be formed: EARTH, HEART, ..
+        // Some English words not part of the list: REATA, ARHAT
+        // Like the previous method, this method may need to wait too.
+        public List<string> AvailableWords(string letters, int len)
+        {
+            letters = letters.ToUpper();
+            //  char[] wordCharArray;
+            List<char> letterList = new List<char>();
+            List<string> availableWordList = new List<string>();
+
+            for (int i = 0; i < englishWordsList.Count; i++)
+            {
+                string word = englishWordsList[i];
+                //   wordCharArray = word.ToCharArray();
+                letterList.AddRange(letters);
+                if (word.Length == len)
+                {
+                    int counter = 0;
+                    for (int j = 0; j < word.Length; j++)
+                    {
+                        if (letterList.Contains(word[j]))
+                        {
+                            letterList.Remove(word[j]);
+                            counter++;
+                        }
+                    }
+                    if (counter == len)
+                    {
+                        availableWordList.Add(word);
+
+                    }
+
+                }//end if
+                letterList.Clear();
+            }//end for loop
+            return availableWordList;
+        }
+        //Takes in a string of letters and a length
+        //Will return a list of all available words with specifid length that
+        //contain letters from the parameter
+        //Will accept duplicates
+        public List<string> AvailableWordsDuplicate(string letters, int len)
+        {
+            letters = letters.ToUpper();
+            //  char[] wordCharArray;
+            List<char> letterList = new List<char>();
+            List<string> availableWordList = new List<string>();
+
+            //For each word
+            for (int i = 0; i < englishWordsList.Count; i++)
+            {
+                string word = englishWordsList[i];
+             
+                letterList.AddRange(letters);
+
+                if (word.Length == len)
+                {
+                    int counter = 0;
+                    for (int j = 0; j < word.Length; j++)
+                    {
+                        if (letterList.Contains(word[j]))
+                        {
+                           // letterList.Remove(word[j]);
+                            counter++;
+                        }
+                    }
+                    if (counter == len)
+                    {
+                        availableWordList.Add(word);
+
+                    }
+
+                }//end if
+                letterList.Clear();
+            }//end for loop
+            return availableWordList;
+        }
+
+    }//End class
+   
+   
+
+}//End namespace
---
+++ Project2/MainPage.cs
@@ -0,0 +1,138 @@
+﻿/*Sarah Kollins
+ *iOS iPhone 8
+ *Using synchronous
+ * What works: Everythings works. 
+ * What doesn't work: Nothing
+ * Assumption: User has to specify a word length before hitting "Find". If they don't, an error occurs.
+ */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Reflection;
+using Xamarin.Forms;
+
+namespace Project2
+{
+    public class MainPage: ContentPage
+    {
+        List<string> wordList = new List<string>();
+        EnglishWords englishWords;
+        Entry wordEntry;
+        Entry letterNumberEntry;
+        ScrollView scrollView;
+        StackLayout bottomLayout;
+        Label wordsLabel = new Label { Text = "" , FontSize=17, HorizontalOptions = LayoutOptions.Center};
+        CheckBox checkBox;
+        StackLayout titleLayout = new StackLayout();
+        Button findButton = new Button { Text = "Find", HorizontalOptions = LayoutOptions.End, Margin = new Thickness(0, 0, 0, 0) };
+        Button clearButton = new Button { Text = "Clear", HorizontalOptions = LayoutOptions.End, Margin = new Thickness(20, 0, 0, 0) };
+
+        //Constructor
+        public MainPage()
+        {
+           
+           englishWords = new EnglishWords();
+            wordList = englishWords.getWordList();
+
+            this.Padding = new Thickness(5,21,5,2);
+            //Creation of the stacklayouts
+            StackLayout stackLayout = new StackLayout();
+            StackLayout topLayout = new StackLayout { Orientation = StackOrientation.Horizontal,
+                Margin = new Thickness(0, 0, 10, 20)};
+            StackLayout middleLayout = new StackLayout { Orientation = StackOrientation.Horizontal };
+           bottomLayout  = new StackLayout();
+
+            //Formatting the title label
+            Label title = new Label { Text = "Welcome to WordFinder", Margin = new Thickness(0,20,0,10), HorizontalOptions = LayoutOptions.Center
+            , FontSize = 30};
+            titleLayout.Children.Add(title);
+            titleLayout.BackgroundColor = Color.PowderBlue;
+           
+            //Formatting the entry of letters
+            wordEntry = new Entry { Text = ""};
+            
+            wordEntry.WidthRequest = 200;
+            Label dupLabel = new Label { Text = "Duplicate Letters?" };
+            checkBox = new CheckBox();
+            checkBox.Color = Color.PowderBlue;
+           
+            topLayout.Children.Add(wordEntry);
+            topLayout.Children.Add(dupLabel);
+            topLayout.Children.Add(checkBox);
+
+            //Formatting the word length entry
+            Label wordLengthLabel = new Label { Text = "Enter in word length:"};
+            letterNumberEntry = new Entry ();
+            letterNumberEntry.WidthRequest = 40;
+            middleLayout.Children.Add(wordLengthLabel);
+            middleLayout.Children.Add(letterNumberEntry);
+         
+            //Formatting the buttons
+            findButton.FontSize = 20;
+            findButton.TextColor = Color.White;
+            findButton.BackgroundColor = Color.PowderBlue;
+            findButton.Clicked += OnClick;
+            clearButton.FontSize = 20;
+            clearButton.TextColor = Color.White;
+            clearButton.BackgroundColor = Color.PowderBlue;
+            clearButton.Clicked += OnClick;
+
+
+            scrollView = new ScrollView();
+            scrollView.Content = bottomLayout;
+           
+            //Adding all content to main stack layout
+            stackLayout.Children.Add(titleLayout);
+            stackLayout.Children.Add(clearButton);
+            stackLayout.Children.Add(topLayout);
+            stackLayout.Children.Add(middleLayout);
+            stackLayout.Children.Add(findButton);
+            stackLayout.Children.Add(scrollView);
+            this.Content = stackLayout;
+          
+        }
+
+        //Event when "Clear" or "Find" is clicked
+        //Will either clear the page or search for available words
+        private void OnClick(object sender, EventArgs e)
+        { 
+            Button button = (Button)sender;
+            if (button.Text == "Find")
+            {
+                if(wordsLabel.Text != "")
+                {
+                    wordsLabel.Text = "";
+                    wordList.Clear();
+                }
+                if (checkBox.IsChecked == false) {
+                    wordList = englishWords.AvailableWords(wordEntry.Text, Int32.Parse(letterNumberEntry.Text));
+                    foreach (String word in wordList)
+                    {
+                        wordsLabel.Text += "\n" + word;
+
+                    }
+                }
+                //For duplicate letters
+                else
+                {
+                    wordList = englishWords.AvailableWordsDuplicate(wordEntry.Text, Int32.Parse(letterNumberEntry.Text));
+                    foreach (String word in wordList)
+                    {
+                        wordsLabel.Text += "\n" + word;
+
+                    }
+                }
+            }
+            //For clear button
+            else
+            {
+                wordEntry.Text = "";
+                letterNumberEntry.Text = "";
+                wordList.Clear();
+                wordsLabel.Text = "";
+            }
+            bottomLayout.Children.Add(wordsLabel);
+        }
+
+    }//End Class
+}//End namespace
